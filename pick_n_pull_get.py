d0b4f8a31ad8975e4c35b2dcd9492db12a359546
Eric Meas <emmeas@gmail.com>
2019-07-16 16:44:54 -0700

    Update pick_n_pull_get.py
    added link variables

--- pick_n_pull_get.py
+++ pick_n_pull_get.py
@@ -4,3 +4,7 @@ import webbrowser
 
 import requests
 from requests.exceptions import HTTPError
+
+link = ("https://www.picknpull.com/check_inventory.aspx?Zip=94578&"
+        "Make=234&Model=4352&Year=&Distance=25")
+chrome_loc = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
