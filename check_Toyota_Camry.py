e883e8c41304fe4148586caf743f4b1ae801cfd3
Eric Meas <emmeas@gmail.com>
2022-04-08 15:28:15 -0700

    Update check_Toyota_Camry.py
    edited link and chrome_loc so they work correctly

--- check_Toyota_Camry.py
+++ check_Toyota_Camry.py
@@ -5,9 +5,9 @@ import webbrowser
 import requests
 from requests.exceptions import HTTPError
 
-link = ("https://www.picknpull.com/check_inventory.aspx?Zip=94578&"
-        "Make=234&Model=4352&Year=&Distance=25")
-chrome_loc = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+link = ("https://www.picknpull.com/check-inventory/vehicle-search?"
+        "make=234&model=4352&distance=25&zip=94578&year=")
+chrome_loc = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
 
 for url in [link]:
   try:
